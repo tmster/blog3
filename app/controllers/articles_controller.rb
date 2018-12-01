7c9c1d67359ea9445b4060f62ea535beedaf1418
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-12-01 12:32:39 +0100

    Modify redirect to show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
     article_params = params.require(:article).permit(:title, :text)
     @article = Article.new(article_params)
     @article.save
-    redirect_to articles_path
+    redirect_to article_path(@article)
   end
 
   def show
