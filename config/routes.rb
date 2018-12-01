baebfb7d4a9cd31887180566827dc8ff4b62e090
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-12-01 11:24:23 +0100

    Create simple for for article

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
