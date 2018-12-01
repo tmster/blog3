576d469a9cbc4f111d8ec1cbe0ed1d4115054a56
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-12-01 14:58:13 +0100

    Add edit option for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    article_params = params.require(:article).permit(:title, :text)
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 end
