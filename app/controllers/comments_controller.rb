9725a5756027f64db35ee2e5e0cd0437f4a3c48b
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-12-02 12:16:53 +0100

    Use nested resources for article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :find_article
+
   def create
     @comment = Comment.new(comment_params)
+    @comment.article = @article
     @comment.save
 
-    redirect_to article_path(@comment.article)
+    redirect_to article_path(@article)
   end
 
   private
 
+  def find_article
+    @article = Article.find(params[:article_id])
+  end
+
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :article_id)
+    params.require(:comment).permit(:commenter, :body)
   end
 end
