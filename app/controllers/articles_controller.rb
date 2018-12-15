5a357b97ca28e870a20774526420870251421abc
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-12-15 12:10:50 +0100

    Add tags feature for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :tags)
   end
 
   def find_article
